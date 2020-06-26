f4dd93e6f6f7d07e87f722a22dd02df7540f881a
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-06-26 11:59:41 -0500

    Linked GET

--- src/redux/sagas/itemsSaga.js
+++ src/redux/sagas/itemsSaga.js
@@ -1,32 +1,49 @@
 import axios from "axios";
 import { put, takeLatest } from "redux-saga/effects";
 
-// function* fetchItems() {
-//   try {
-//     const config = {
-//       headers: { "Content-Type": "application/json" },
-//       withCredentials: true,
-//     };
-//     const response = yield axios.get("/api/item", config);
-//     yield put({
-//       type: "SET_ITEMS",
-//       payload: response.data,
-//     });
-//   } catch (error) {
-//     console.log("Item GET in itemSaga.js failed", error);
-//   }
-// }
+function* getItem() {
+  console.log("in GET ITEM");
+  try {
+    const response = yield axios.get(`/api/item`);
+    yield put({
+      type: "SET_ITEMS_LIST",
+      payload: response.data,
+    });
+    console.log(response);
+  } catch (error) {
+    console.log("axios GET err", error);
+  }
+}
 
+/* function* fetchItems() {
+  try {
+    const config = {
+      headers: { "Content-Type": "application/json" },
+      withCredentials: true,
+    };
+    const response = yield axios.get("/api/item", config);
+    yield put({
+      type: "SET_ITEMS_LIST",
+      payload: response.data,
+    });
+  } catch (error) {
+    console.log("Item GET in itemSaga.js failed", error);
+  }
+}
+ */
 function* itemsSaga() {
-//   yield takeLatest("FETCH_ITEMS", fetchItems);
-  yield takeLatest("SET_ITEMS", addItems);
+  yield takeLatest("GET_ITEMS_LIST", getItem);
+  yield takeLatest("ADD_ITEMS", addItems);
 }
 
 function* addItems(action) {
-    console.log(action);
+  try {
+    yield axios.post(`/api/item`, action.payload);
+    yield put({ type: "GET_ITEMS_LIST" });
     console.log(action.payload);
-  yield axios.post(`/api/item`, action.payload);
-  put({ type: "SET_ITEMS_LIST" });
+  } catch (error) {
+    console.log("Item POST err", error);
+  }
 }
 
 export default itemsSaga;
