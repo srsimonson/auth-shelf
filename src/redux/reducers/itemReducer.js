133983e7fc0742740d4fa7c72e2ffa0d42bb69f4
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-06-26 09:39:06 -0500

    Input complete

--- src/redux/reducers/itemReducer.js
+++ src/redux/reducers/itemReducer.js
@@ -1,14 +1,12 @@
 const itemReducer = (state = [], action) => {
   switch (action.type) {
-    case "SET_ITEM":
+    case "SET_ITEMS":
       return action.payload;
-    case "UNSET_ITEM":
+    case "UNSET_ITEMS":
       return [];
     default:
       return state;
   }
 };
 
-// user will be on the redux state at:
-// state.user
 export default itemReducer;
